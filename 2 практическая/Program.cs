4e7d7c2243506329a165b49fbebea70bba19716f
Vladislav301 <gordeevvlad151@gmail.com>
2024-11-19 16:12:52 +0900

    Add files via upload

---
+++ 2 практическая/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class AccessModifierExample
+{
+   
+    public int Add(int x, int y)
+    {
+        return x + y;
+    }
+
+   
+    private void PrintResult(int result)
+    {
+        Console.WriteLine($"Результат: {result}");
+    }
+
+    public static void Main(string[] args)
+    {
+        AccessModifierExample example = new AccessModifierExample();
+
+       
+        int sum = example.Add(7, 3);
+        Console.WriteLine($"Сумма 7 + 3 = {sum}"); 
+
+       
+        example.PrintResult(sum);
+    }
+    
+
+}
